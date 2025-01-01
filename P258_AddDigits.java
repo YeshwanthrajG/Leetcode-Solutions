4ddef9fe02263f23aea240cd45ff44599c8526f5
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-01-01 20:03:03 +0530

    Leetcode problem 258 solved using java

---
+++ P258_AddDigits.java
@@ -0,0 +1,26 @@
+/* 
+Problem ID : 258 
+
+Problem : Add Digits
+
+Statement : Given an integer num, repeatedly add all its digits until the 
+result has only one digit, and return it
+*/
+
+/* Problem Link
+https://leetcode.com/problems/add-digits/description/
+*/
+
+class Solution {
+    public int addDigits(int num) {
+        if(num<10)
+            return num;
+
+        int temp=(num%10)+addDigits(num/10);
+
+        if(temp>9)
+            temp=(temp%10)+addDigits(temp/10);
+
+        return temp;
+    }
+}
