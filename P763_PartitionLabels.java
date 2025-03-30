71c53b2fbcb291eef522ea31e6106c8d7d8f1ed7
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-03-30 18:47:23 +0530

    Leetcode problem 763 solved using java

---
+++ P763_PartitionLabels.java
@@ -0,0 +1,40 @@
+/* 
+Problem ID : 763 
+
+Problem : Partition Labels
+
+Statement : You are given a string s. We want to partition the string into as many parts as possible so that 
+each letter appears in at most one part. For example, the string "ababcc" can be partitioned into 
+["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string 
+should be s.
+
+Return a list of integers representing the size of these parts.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/partition-labels/description/?envType=daily-question&envId=2025-03-30
+*/
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence.put(s.charAt(i), i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
