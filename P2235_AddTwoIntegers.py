7b19a58e2ce1eafda15eab9cb581037bfdc1d075
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-06-25 22:57:38 +0530

    Leetcode problem 2235 solved using python

---
+++ P2235_AddTwoIntegers.py
@@ -0,0 +1,16 @@
+"""
+Problem ID : 2235 
+
+Problem : Add Two Integers
+
+Statement : Given two integers num1 and num2, return the sum of the two integers.
+"""
+
+""" Problem Link
+https://leetcode.com/problems/add-two-integers/description/
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
+        
