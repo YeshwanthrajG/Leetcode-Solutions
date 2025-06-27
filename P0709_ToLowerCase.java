52c5c352c45e5e18c4ce9f9f04ff70dcb939b751
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-06-27 14:30:47 +0530

    Leetcode problem 709 solved using java

---
+++ P0709_ToLowerCase.java
@@ -0,0 +1,17 @@
+/* 
+Problem ID : 709 
+
+Problem : To Lower Case
+
+Statement : Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/to-lower-case/description/
+*/
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
