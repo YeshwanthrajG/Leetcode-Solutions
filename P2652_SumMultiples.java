bd27af2596429b3903c4694b7384468b0c4b13bc
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-06-27 14:26:52 +0530

    Leetcode problem 2652 solved using java

---
+++ P2652_SumMultiples.java
@@ -0,0 +1,24 @@
+/* 
+Problem ID : 2652 
+
+Problem : Sum Multiples
+
+Statement : Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are 
+undivisible by 3, 5, or 7.
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/construct-k-palindrome-strings/description/?envType=daily-question&envId=2025-01-11
+*/
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0)
+                sum+=i;
+        }
+        return sum;
+    }
+}
