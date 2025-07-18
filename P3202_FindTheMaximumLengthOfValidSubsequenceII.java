a91ecebb137353a2eb46af28fba3bf5c0adaac77
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-07-18 20:16:50 +0530

    Leetcode problem 3202 solved using java

--- P3202_FindTheMaximumLengthOfValidSubsequenceII.java
+++ P3202_FindTheMaximumLengthOfValidSubsequenceII.java
@@ -5,8 +5,8 @@ Problem : Find the Maximum Length of Valid Subsequence II
 
 Statement : You are given an integer array nums and a positive integer k.
 A subsequence sub of nums with length x is called valid if it satisfies:
-
 (sub[0] + sub[1]) % k == (sub[1] + sub[2]) % k == ... == (sub[x - 2] + sub[x - 1]) % k.
+
 Return the length of the longest valid subsequence of nums.
 */
 
