05f833a14c799c5145ab72e61c075ea44d7bd7a0
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-07-22 22:54:05 +0530

    Leetcode problem 1957 solved using java

--- P1957_DeleteCharactersToMakeFancyString.java
+++ P1957_DeleteCharactersToMakeFancyString.java
@@ -4,9 +4,7 @@ Problem ID : 1957
 Problem : Delete Characters to Make Fancy String
 
 Statement : A fancy string is a string where no three consecutive characters are equal.
-
 Given a string s, delete the minimum possible number of characters from s to make it fancy.
-
 Return the final string after the deletion. It can be shown that the answer will always be unique.
 */
 
