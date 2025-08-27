85f6a8e00f2679d37797a819fb0914d0c409dfa9
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-08-27 21:30:21 +0530

    Leetcode problem 3658 solved using java

---
+++ P3658_GCDOfOddAndEvenSums.java
@@ -0,0 +1,23 @@
+/* 
+Problem ID : 3658 
+
+Problem : GCD of Odd and Even Sums
+
+Statement : You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+
+sumOdd: the sum of the first n odd numbers.
+
+sumEven: the sum of the first n even numbers.
+
+Return the GCD of sumOdd and sumEven.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/gcd-of-odd-and-even-sums/description/
+*/
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
