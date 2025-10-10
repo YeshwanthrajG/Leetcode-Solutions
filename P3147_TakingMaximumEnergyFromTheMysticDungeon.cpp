b108b6ff8e0f1938f1b9b24d03859257006e72bf
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-10-10 18:06:38 +0530

    Leetcode problem 3147 solved using cpp

---
+++ P3147_TakingMaximumEnergyFromTheMysticDungeon.cpp
@@ -0,0 +1,38 @@
+/* 
+Problem ID : 3147 
+
+Problem : Taking Maximum Energy From the Mystic Dungeon
+
+Statement : In a mystic dungeon, n magicians are standing in a line. Each magician has an attribute that
+ gives you energy. Some magicians can give you negative energy, which means taking energy from you.
+
+You have been cursed in such a way that after absorbing energy from magician i, you will be instantly 
+transported to magician (i + k). This process will be repeated until you reach the magician where (i + k) 
+does not exist.
+
+In other words, you will choose a starting point and then teleport with k jumps until you reach the end of 
+the magicians' sequence, absorbing all the energy during the journey.
+
+You are given an array energy and an integer k. Return the maximum possible energy you can gain.
+
+Note that when you are reach a magician, you must take energy from them, whether it is negative or 
+positive energy.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/description/?envType=daily-question&envId=2025-10-10
+*/
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n=energy.size();
+        vector<int> dp(n);
+        int result=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=energy[i]+((i+k<n)?dp[i+k]:0);
+            result=max(result,dp[i]);
+        }
+        return result;
+    }
+};
