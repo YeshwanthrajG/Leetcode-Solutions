8380a794d65016279477845eb0ad1a62728c9a2c
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-10-18 22:17:09 +0530

    Leetcode problem 3397 solved using cpp

---
+++ P3397_MaximumNumberOfDistinctElementsAfterOperations.cpp
@@ -0,0 +1,32 @@
+/* 
+Problem ID : 3397 
+
+Problem : Maximum Number of Distinct Elements After Operations
+
+Statement : You are given an integer array nums and an integer k.
+
+You are allowed to perform the following operation on each element of the array at most once:
+
+Add an integer in the range [-k, k] to the element.
+Return the maximum possible number of distinct elements in nums after performing the operations.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/description/?envType=daily-question&envId=2025-10-18
+*/
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, prev = -1e9;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int l = max(nums[i] - k, prev + 1);
+            if (l <= nums[i] + k) {
+                prev = l, ans++;
+            }
+        }
+        return ans;
+    }
+};
