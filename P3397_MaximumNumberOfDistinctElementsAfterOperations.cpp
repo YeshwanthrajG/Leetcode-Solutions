2753204ad1d8ef0739a7427e68357839b7b9a215
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-10-19 21:56:29 +0530

    Leetcode problem 3397 solved using cpp

--- P3397_MaximumNumberOfDistinctElementsAfterOperations.cpp
+++ P3397_MaximumNumberOfDistinctElementsAfterOperations.cpp
@@ -20,7 +20,6 @@ public:
     int maxDistinctElements(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int ans = 0, prev = -1e9;
-
         for (int i = 0; i < nums.size(); i++) {
             int l = max(nums[i] - k, prev + 1);
             if (l <= nums[i] + k) {
