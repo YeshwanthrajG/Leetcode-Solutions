5bd4e76ea3321661666f72ac703e9d92ed559f07
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-10-30 19:56:18 +0530

    Leetcode problem 3370 solved using java

--- P3370_SmallestNumberWithAllSetBits.java
+++ P3370_SmallestNumberWithAllSetBits.java
@@ -5,7 +5,7 @@ Problem : Smallest Number With All Set Bits
 
 Statement : You are given a positive number n.
 
-Return the smallest number x greater than or equal to n, such that the binary representation of x 
+Return the smallest number x greater than or equal to n, such that the binary representation of x
 contains only set bits
 */
 
