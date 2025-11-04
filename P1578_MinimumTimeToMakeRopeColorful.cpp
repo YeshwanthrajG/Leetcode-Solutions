448d3e3128c82c83862d2ede71284b72196a073a
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-11-04 20:24:27 +0530

    Leetcode problem 1578 solved using cpp

--- P1578_MinimumTimeToMakeRopeColorful.cpp
+++ P1578_MinimumTimeToMakeRopeColorful.cpp
@@ -10,7 +10,6 @@ Alice wants the rope to be colorful. She does not want two consecutive balloons 
 so she asks Bob for help. Bob can remove some balloons from the rope to make it colorful. 
 You are given a 0-indexed integer array neededTime where neededTime[i] is the time (in seconds) that 
 Bob needs to remove the ith balloon from the rope.
-
 Return the minimum time Bob needs to make the rope colorful. 
 */
 
