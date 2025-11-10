027e97d915c72de84e9045badcd594376f0f99fc
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-11-10 22:27:43 +0530

    Update P2616_MinimizeTheMaximumDifferenceOfPairs.java

--- P2616_MinimizeTheMaximumDifferenceOfPairs.java
+++ P2616_MinimizeTheMaximumDifferenceOfPairs.java
@@ -4,8 +4,7 @@ Problem ID : 2616
 Problem : Minimize the Maximum Difference of Pairs
 
 Statement : You are given a 0-indexed integer array nums and an integer p. Find p pairs of indices of nums 
-such that the maximum difference amongst all the pairs is minimized. Also, ensure no index appears more than 
-once amongst the p pairs.
+such that the maximum difference amongst all the pairs is minimized. Also, ensure no index appears more than once amongst the p pairs.
 
 Note that for a pair of elements at the index i and j, the difference of this pair is |nums[i] - nums[j]|, 
 where |x| represents the absolute value of x.
