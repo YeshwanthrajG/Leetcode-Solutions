d35ac4312aed9bb680692c23ace9489b2d6f74cd
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-11-20 13:49:58 +0530

    Leetcode problem 717 solved using cpp

---
+++ P0717_1bitand2bitCharacters.cpp
@@ -0,0 +1,25 @@
+/* 
+Problem ID : 717 
+
+Problem : 1-bit and 2-bit Characters
+
+Statement : We have two special characters:
+
+The first character can be represented by one bit 0.
+The second character can be represented by two bits (10 or 11).
+Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/1-bit-and-2-bit-characters/description/?envType=daily-question&envId=2025-11-20
+*/
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n=bits.size();
+        int i=0;
+        while(i<n-1) i+=bits[i]+1;
+        return i==n-1;
+    }
+};
