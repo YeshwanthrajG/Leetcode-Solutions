ff25271d4ece0c5503323efb784e9a5caa07b1c1
YeshwanthrajG <yeshwanth1155@gmail.com>
2025-11-23 20:57:17 +0530

    Leetcode problem 3190 solved using cpp

--- P3190_FindMinimumOperationsToMakeAllElementsDivisibleByThree.cpp
+++ P3190_FindMinimumOperationsToMakeAllElementsDivisibleByThree.cpp
@@ -3,8 +3,8 @@ Problem ID : 3190
 
 Problem : Find Minimum Operations to Make All Elements Divisible by Three
 
-Statement : You are given an integer array nums. In one operation, you can add or subtract 1 
-from any element of nums.
+Statement : You are given an integer array nums. 
+In one operation, you can add or subtract 1 from any element of nums.
 
 Return the minimum number of operations to make all elements of nums divisible by 3.
 */
