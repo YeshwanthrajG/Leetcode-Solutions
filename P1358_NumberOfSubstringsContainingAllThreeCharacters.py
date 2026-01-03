69f5877c26800b0ff5ec2e3573425c7958961b13
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-01-03 23:11:49 +0530

    Update P1358_NumberOfSubstringsContainingAllThreeCharacters.py

--- P1358_NumberOfSubstringsContainingAllThreeCharacters.py
+++ P1358_NumberOfSubstringsContainingAllThreeCharacters.py
@@ -20,6 +20,5 @@ class Solution:
             last[ord(s[i])-ord('a')]=i
             if last[0]!=-1 and last[1]!=-1 and last[2]!=-1:
                 count+=(1+min(last[0],last[1],last[2]))
-
         return count
         
