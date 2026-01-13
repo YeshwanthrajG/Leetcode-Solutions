81270a54884acdfdbd65be74f4bc9fc2a34d1b35
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-01-13 20:24:34 +0530

    Leetcode problem 1266 solved using cpp

---
+++ P1266_MinimumTimeVisitingAllPoints.cpp
@@ -0,0 +1,33 @@
+/* 
+Problem ID : 1266 
+
+Problem : Minimum Time Visiting All Points
+
+Statement : On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. 
+Return the minimum time in seconds to visit all the points in the order given by points.
+
+You can move according to these rules:
+
+In 1 second, you can either:
+move vertically by one unit,
+move horizontally by one unit, or
+move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally 
+in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/minimum-time-visiting-all-points/description/?envType=daily-question&envId=2026-01-13
+*/
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int d=0;
+        for(int i=0;i<points.size()-1;i++){
+            d+=max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1]));
+        }
+        return d;
+    }
+};
