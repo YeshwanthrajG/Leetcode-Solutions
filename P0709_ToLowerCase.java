6f32767be4d20c5dce9b1482fd7d333a07c22a11
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-01-24 22:43:03 +0530

    Update P0709_ToLowerCase.java

--- P0709_ToLowerCase.java
+++ P0709_ToLowerCase.java
@@ -12,6 +12,6 @@ https://leetcode.com/problems/to-lower-case/description/
 
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        return s.toLowerCase(); 
     }
 }
