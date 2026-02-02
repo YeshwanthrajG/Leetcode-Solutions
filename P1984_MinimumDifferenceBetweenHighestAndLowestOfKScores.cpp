6bdda0d6bff3aa1223e627f088d43279e67fd373
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-02-02 21:34:26 +0530

    Leetcode problem 1984 solved using cpp

---
+++ P1984_MinimumDifferenceBetweenHighestAndLowestOfKScores.cpp
@@ -0,0 +1,25 @@
+/* 
+Problem ID : 1984 
+
+Problem : Minimum Difference Between Highest and Lowest of K Scores
+
+Statement : You are given a 0-indexed integer array nums, where nums[i] represents the score of 
+the ith student. You are also given an integer k.
+
+Pick the scores of any k students from the array so that the difference between the highest and the 
+lowest of the k scores is minimized.
+
+Return the minimum possible difference.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/?envType=daily-question&envId=2026-01-28
+*/
+
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1) return 0;
+    }
+};
