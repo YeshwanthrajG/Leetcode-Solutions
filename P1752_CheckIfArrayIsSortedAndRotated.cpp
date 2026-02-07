58a937b8dbdc0c75a2ce1e2ab5825be97901244d
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-02-07 20:43:27 +0530

    Leetcode problem 1752 solved using cpp

---
+++ P1752_CheckIfArrayIsSortedAndRotated.cpp
@@ -0,0 +1,32 @@
+/* 
+Problem ID : 1752 
+
+Problem : Check if Array Is Sorted and Rotated
+
+Statement : Given an array nums, return true if the array was originally sorted in non-decreasing order, 
+then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same 
+length such that B[i] == A[(i+x) % A.length] for every valid index i.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
+            }
+            if(count>1) return false;
+        }
+        return true;
+    }
+};
