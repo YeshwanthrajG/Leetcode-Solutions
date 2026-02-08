dc8c93997832903ac03c9754b8e7328df759cdb8
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-02-08 20:34:47 +0530

    Leetcode problem 110 solved using cpp

---
+++ P0110_BalancedBinaryTree.cpp
@@ -0,0 +1,38 @@
+/* 
+Problem ID : 110 
+
+Problem : Balanced Binary Tree
+
+Statement : Given a binary tree, determine if it is height-balanced.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/balanced-binary-tree/description/?envType=daily-question&envId=2026-02-08
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfs(root)!=-1;
+    }
+    int dfs(TreeNode* node){
+        if(!node) return 0;
+        int left=dfs(node->left);
+        if(left==-1) return -1;
+        int right=dfs(node->right);
+        if(right==-1) return -1;
+        if(abs(left-right)>1) return -1;
+        return 1+max(left, right);
+    }
+};
