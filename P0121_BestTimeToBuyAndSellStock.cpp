28e3807c372959db217ff1578f7fb5db45de0435
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-03-25 21:27:05 +0530

    Leetcode problem 121 solved in c++

---
+++ P0121_BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,33 @@
+/* 
+Problem ID : 121 
+
+Problem : Best Time to Buy and Sell Stock
+
+Statement : You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day 
+in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, 
+return 0.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int price=0, minCost=prices[0];
+        for(int i=1;i<n;i++){
+            int temp=prices[i]-minCost;
+            if(temp>price){
+                price=temp;
+            }
+            minCost=min(minCost, prices[i]);
+        }
+        return price;
+    }
+};
