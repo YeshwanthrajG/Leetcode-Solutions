84e2d277ddefdbfe0fb3d80c21240f69420b32bd
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-03-26 22:51:31 +0530

    Leetcode problem 69 solved in c++

---
+++ P0069_SqrtX.cpp
@@ -0,0 +1,19 @@
+/* 
+Problem ID : 69 
+
+Problem : Sqrt(x)
+
+Statement : Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+The returned integer should be non-negative as well.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/sqrtx/description/
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return sqrt(x);
+    }
+};
