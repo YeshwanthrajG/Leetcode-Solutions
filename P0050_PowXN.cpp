c2456450bc1ca577885920fe4e605b7a0160de35
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-03-26 22:50:30 +0530

    Leetcode problem 50 solved in c++

---
+++ P0050_PowXN.cpp
@@ -0,0 +1,18 @@
+/* 
+Problem ID : 50 
+
+Problem : Pow(x, n)
+
+Statement : Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+*/
+
+/* Problem Link
+https://leetcode.com/problems/powx-n/description/
+*/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
