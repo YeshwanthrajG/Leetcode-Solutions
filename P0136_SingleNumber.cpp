17c4bfe0c990e9eede66d55e24d0df556166de49
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-04-27 20:46:32 +0530

    Leetcode problem 136 solved in c++

---
+++ P0136_SingleNumber.cpp
@@ -0,0 +1,23 @@
+/* 
+Problem ID : 136 
+
+Problem : Single Number
+
+Statement : Given a non-empty array of integers nums, every element appears twice except for one. 
+Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/single-number/description/
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int num=nums[0];
+        for(int i=1;i<nums.size();i++) num^=nums[i];
+        return num;
+    }
+};
