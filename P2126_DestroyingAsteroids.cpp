f403170559a2692aaa75e7c8e0441ac355d482e0
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-05-31 11:53:07 +0530

    Leetcode problem 2126 solved in c++

---
+++ P2126_DestroyingAsteroids.cpp
@@ -0,0 +1,32 @@
+/* 
+Problem ID : 2126 
+
+Problem : Destroying Asteroids
+
+Statement : You are given an integer mass, which represents the original mass of a planet. You are 
+further given an integer array asteroids, where asteroids[i] is the mass of the ith asteroid.
+
+You can arrange for the planet to collide with the asteroids in any arbitrary order. If the mass of the 
+planet is greater than or equal to the mass of the asteroid, the asteroid is destroyed and the planet 
+gains the mass of the asteroid. Otherwise, the planet is destroyed.
+
+Return true if all asteroids can be destroyed. Otherwise, return false.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/destroying-asteroids/description/?envType=daily-question&envId=2026-05-31
+*/
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        int n=asteroids.size();
+        long long m=mass;
+        for(int i=0;i<n;i++){
+            if(m<asteroids[i]) return false;
+            m+=asteroids[i];
+        }
+        return true;
+    }
+};
