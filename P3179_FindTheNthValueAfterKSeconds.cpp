c037afac425e36994459ef63d6a41bcb00f2ff84
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-06-12 11:03:06 +0530

    Leetcode problem 3179 solved in c++

---
+++ P3179_FindTheNthValueAfterKSeconds.cpp
@@ -0,0 +1,34 @@
+/* 
+Problem ID : 3179 
+
+Problem : Find the N-th Value After K Seconds
+
+Statement : You are given two integers n and k.
+
+Initially, you start with an array a of n integers where a[i] = 1 for all 0 <= i <= n - 1. 
+After each second, you simultaneously update each element to be the sum of all its preceding elements 
+plus the element itself. For example, after one second, a[0] remains the same, a[1] becomes a[0] + a[1], 
+a[2] becomes a[0] + a[1] + a[2], and so on.
+
+Return the value of a[n - 1] after k seconds.
+
+Since the answer may be very large, return it modulo 109 + 7.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/find-the-n-th-value-after-k-seconds/description/
+*/
+
+class Solution {
+public:
+    int valueAfterKSeconds(int n, int k) {
+        static int MOD=1e9+7;
+        vector<int> res(n, 1);
+        while(k--){
+            for(int i=1;i<n;i++){
+                res[i]=(res[i]+res[i-1])%MOD;
+            }
+        }
+        return res[n-1];
+    }
+};
