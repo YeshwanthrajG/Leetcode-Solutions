0383458d7ef1311d703b9d4239fb1da311a327d5
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-06-23 19:55:43 +0530

    Leetcode problem 31 solved in c++

---
+++ P0031_NextPermutation.cpp
@@ -0,0 +1,54 @@
+/* 
+Problem ID : 31 
+
+Problem : Next Permutation
+
+Statement : A permutation of an array of integers is an arrangement of its members into a sequence or 
+linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], 
+[2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its 
+integer. More formally, if all the permutations of the array are sorted in one container according to 
+their lexicographical order, then the next permutation of that array is the permutation that follows it 
+in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest 
+possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical 
+larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/next-permutation/description/
+*/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int idx1=-1, idx2=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx1=i;
+                break;
+            }
+        }
+        if(idx1==-1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[idx1]<nums[i]){
+                idx2=i;
+                break;
+            }
+        }
+        swap(nums[idx1], nums[idx2]);
+        reverse(nums.begin()+idx1+1, nums.end());
+    }
+};
