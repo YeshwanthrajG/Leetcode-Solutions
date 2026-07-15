38126ed9a6f319aebaa7332c2c90b7304202830b
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-07-15 21:53:18 +0530

    Leetcode problem 3658 solved in c++

---
+++ P3658_GCDOfOddAndEvenSums.cpp
@@ -0,0 +1,25 @@
+/* 
+Problem ID : 3658 
+
+Problem : GCD of Odd and Even Sums
+
+Statement : You are given an integer n. Your task is to compute the GCD (greatest common divisor) of 
+two values:
+
+sumOdd: the sum of the smallest n positive odd numbers.
+
+sumEven: the sum of the smallest n positive even numbers.
+
+Return the GCD of sumOdd and sumEven.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/gcd-of-odd-and-even-sums/description/?envType=daily-question&envId=2026-07-15
+*/
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
