0237a226788b8ee128b24e364f569d3464a3a0a8
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-08-06 21:12:16 +0530

    Leetcode problem 3345 solved in c++

---
+++ P3345_SmallestDivisibleDigitProductI.cpp
@@ -0,0 +1,34 @@
+/* 
+Problem ID : 3345 
+
+Problem : Smallest Divisible Digit Product I
+
+Statement : You are given two integers n and t. Return the smallest number greater than or equal to n 
+such that the product of its digits is divisible by t.
+*/
+
+/* Problem Link
+https://leetcode.com/problems/smallest-divisible-digit-product-i/description/?envType=daily-question&envId=2026-08-06
+*/
+
+class Solution {
+public:
+    int digProd(int n){
+        int res=1;
+        while(n>0){
+            res*=(n%10);
+            n/=10;
+        }
+        return res;
+    }
+
+    int smallestNumber(int n, int t) {
+        int i=n;
+        int p=digProd(i);
+        while(p%t!=0){
+            i++;
+            p=digProd(i);
+        }
+        return i;
+    }
+};
