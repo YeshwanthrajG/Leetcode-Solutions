b3b6fb63093320a4a24cb8c28ca15472f6f4ff86
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-08-21 23:18:42 +0530

    Update P0238_ProductOfArrayExceptSelf.cpp

--- P0238_ProductOfArrayExceptSelf.cpp
+++ P0238_ProductOfArrayExceptSelf.cpp
@@ -20,7 +20,7 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
         vector<int> res(n,1);
-        int left=1;
+        int left=1; 
         for(int i=0;i<n;i++){
             res[i]*=left;
             left*=nums[i];
