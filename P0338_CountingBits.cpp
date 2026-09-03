27d071a1aa8d5abdcfe2844dc6a4cce6a7e14706
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-09-03 21:09:39 +0530

    Update P0338_CountingBits.cpp

--- P0338_CountingBits.cpp
+++ P0338_CountingBits.cpp
@@ -3,8 +3,7 @@ Problem ID : 338
 
 Problem : Counting Bits
 
-Statement : Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
-ans[i] is the number of 1's in the binary representation of i.
+Statement : Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 */
 
 /* Problem Link
