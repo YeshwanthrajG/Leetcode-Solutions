718c5d6a0577303e30990463944d0646e6a71943
YeshwanthrajG <yeshwanth1155@gmail.com>
2026-09-17 21:17:27 +0530

    Leetcode problem 23 solved in c++

--- P0023_MergekSortedLists.cpp
+++ P0023_MergekSortedLists.cpp
@@ -22,6 +22,9 @@ https://leetcode.com/problems/merge-k-sorted-lists/description/
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+ // Approach 1
 class Solution {
 public:
     ListNode* merge(vector<ListNode*> &lists, int start, int end){
@@ -54,5 +57,52 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.empty()) return NULL;
         return merge(lists, 0, lists.size()-1);
+    }
+};
+
+
+
+// Approach 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(n==0) return nullptr;
+        if(n==1) return lists[0];
+        for(int i=1;i<n;i++){
+            lists[i]=mergeTwoLists(lists[i], lists[i-1]);
+        }
+        return lists.back();
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        if(!list1) return list2;
+        if(!list2) return list1;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* curr=dummy;
+        while(list1 && list2){
+            if(list1->val<=list2->val){
+                curr->next=list1;
+                list1=list1->next;
+            }
+            else{
+                curr->next=list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
+        }
+        if(list1) curr->next=list1;
+        if(list2) curr->next=list2;
+        return dummy->next;
     }
 };
